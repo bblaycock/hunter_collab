9624dfa2ee2cf0e5826e2c5aa241824cfb870d72
hunterlaycock <ajplah@gmail.com>
2023-06-13 17:41:37 -0400

    changed value of x

--- type_example.py
+++ type_example.py
@@ -1,8 +1,8 @@
 #this is and example of an integer variable
 ##this will declare a variable anmed x and assign an integer of 1
-x=1
+x=100
 #this will just print a string to the screen The value of x is:
 
-print("The value of x is:")
+print("The value of x was:")
 ##this will print the acutal value of the variable x to the screen
 print(x)
